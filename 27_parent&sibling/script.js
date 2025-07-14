a3bd78ac03d3fca438d54d3a975939b1833b89d8
coderVIP13 <vidyanshoncrease@gmail.com>
2025-07-14 23:14:01 +0530

    .

--- 27_parent&sibling/script.js
+++ 27_parent&sibling/script.js
@@ -1,1 +1,9 @@
 console.log(document.body.firstChild);
+let a = document.body.firstChild
+console.log(a.parentElement);
+console.log(a.parentNode);
+console.log(a.firstChild.nextSibling)
+
+// when we need to onnly move elements
+// element only
+
