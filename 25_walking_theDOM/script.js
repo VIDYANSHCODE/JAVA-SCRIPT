f4b96b99de495a5036a195f49849e8b896ee85c6
hackeroncrease <vidyanshupatel_ee24a19_046@dtu.ac.in>
2025-07-14 07:43:16 +0000

    add all practise sets

---
+++ 25_walking_theDOM/script.js
@@ -0,0 +1,4 @@
+console.log(document.body); // returns body tag
+console.log(typeof document.body);
+console.log(document.documentElement); // returs html tag
+console.log(document.head); // returns head tag
