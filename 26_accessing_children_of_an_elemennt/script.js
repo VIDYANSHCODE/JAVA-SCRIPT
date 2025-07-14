20f73be7121633235bfc441970068d73a928f194
hackeroncrease <vidyanshupatel_ee24a19_046@dtu.ac.in>
2025-07-14 07:47:40 +0000

    .

--- 26_accessing_children_of_an_elemennt/script.js
+++ 26_accessing_children_of_an_elemennt/script.js
@@ -1,3 +1,3 @@
-console.log(document.body.firstChild)
-console.log(document.body.lastChild)
-console.log(document.body.childNodes)
+console.log(document.body.firstChild);
+console.log(document.body.lastChild);
+console.log(document.body.childNodes + typeof document.body.childNodes);
